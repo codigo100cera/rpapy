6d0bb99258852738f222c3d6d6cccbe516bd785d
McSilva <microrepar@gmail.com>
2024-03-17 11:50:04 -0300

    feat: adds new tag version 1.0.6

--- rpapy/__init__.py
+++ rpapy/__init__.py
@@ -1,3 +1,3 @@
 
 # Version of rpapy package
-__version__ = "1.0.5"
+__version__ = "1.0.6"
--- rpapy/__main__.py
+++ rpapy/__main__.py
@@ -1,7 +1,7 @@
 """RPAPY is a open source easy tool for automating boring stuffs on any screen with robotframework, pyautogui, pywinauto and others.
 --------
 
-- rpapy v1.0.5
+- rpapy v1.0.6
 """
 import sys
 from pathlib import Path
