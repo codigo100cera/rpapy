751b6ddbecebe32d161a8ff02527370576074d7f
McSilva <microrepar@gmail.com>
2024-03-22 18:03:50 -0300

    fix: adds new enviroment variable - version 1.0.8

--- rpapy/__main__.py
+++ rpapy/__main__.py
@@ -1,7 +1,7 @@
 """RPAPY is a open source easy tool for automating boring stuffs on any screen with robotframework, pyautogui, pywinauto and others.
 --------
 
-- rpapy v1.0.7
+- rpapy v1.0.8
 """
 import sys
 from pathlib import Path
