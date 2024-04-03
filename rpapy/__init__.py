828762d41e8d684bdbc75cc3d5921b99ffbc175e
McSilva <microrepar@gmail.com>
2024-04-03 16:15:13 -0300

    refact: adds new version tag 1.1.3

--- rpapy/__init__.py
+++ rpapy/__init__.py
@@ -1,3 +1,3 @@
 
 # Version of rpapy package
-__version__ = "1.1.2"
+__version__ = "1.1.3"
--- rpapy/__main__.py
+++ rpapy/__main__.py
@@ -1,7 +1,7 @@
 """RPAPY is a open source easy tool for automating boring stuffs on any screen with robotframework, pyautogui, pywinauto and others.
 --------
 
-- rpapy v1.1.2
+- rpapy v1.1.3
 """
 import sys
 from pathlib import Path
